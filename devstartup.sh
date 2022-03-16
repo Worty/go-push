1a98c9d03373576c4fb333c8199f934910484d01
Worty <6840978+Worty@users.noreply.github.com>
2022-03-16 14:59:32 +0100

    Devstart forward to google

--- devstartup.sh
+++ devstartup.sh
@@ -2,4 +2,4 @@
 go install github.com/codegangsta/gin@latest
 go mod download
 
-PUSHUSER="user" PUSHPW="pw" HOST="localhost" FORWARDSITE="https://worty.de" FORWARDSITE="https://worty.de" gin -a 8080 -p 8081
+PUSHUSER="user" PUSHPW="pw" HOST="localhost" FORWARDSITE="https://google.de" gin -a 8080 -p 8081
